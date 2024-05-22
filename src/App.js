3c15d525f8d418a7154eaf8032ece04ea9c38c94
zhangjun <zhangjunsz@126.com>
2024-05-22 14:25:10 +0800

    添加到react demo 中

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import HookCakeContainer from './components/HookCakeContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <CakeContainer />
+      <HookCakeContainer />
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
